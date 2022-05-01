1a093cbe38011b0a7a70a9bf3eee1358c27e6a3e
FMuenke <friedrich.muenke@me.com>
2022-05-01 16:11:01 +0200

    updated run script

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 model_name="cipp"
 
-python multi_train.py -df ~/datasets/synth_000/ --model ~/ai_models/synth_000/$model_name
-python multi_train.py -df ~/datasets/synth_A001/ --model ~/ai_models/synth_A001/$model_name
-python multi_train.py -df ~/datasets/synth_A002/ --model ~/ai_models/synth_A002/$model_name
-python multi_train.py -df ~/datasets/synth_A003/ --model ~/ai_models/synth_A003/$model_name
-python multi_train.py -df ~/datasets/synth_A004/ --model ~/ai_models/synth_A004/$model_name
-python multi_train.py -df ~/datasets/synth_A005/ --model ~/ai_models/synth_A005/$model_name
-python multi_train.py -df ~/datasets/synth_A010/ --model ~/ai_models/synth_A010/$model_name
-python multi_train.py -df ~/datasets/synth_A015/ --model ~/ai_models/synth_A015/$model_name
-python multi_train.py -df ~/datasets/synth_A020/ --model ~/ai_models/synth_A020/$model_name
-python multi_train.py -df ~/datasets/synth_A025/ --model ~/ai_models/synth_A025/$model_name
-python multi_train.py -df ~/datasets/synth_A050/ --model ~/ai_models/synth_A050/$model_name
-python multi_train.py -df ~/datasets/synth_A100/ --model ~/ai_models/synth_A100/$model_name
+python multi_train.py -df ~/datasets/synth_000/ --model ~/ai_models/synth_000_$model_name
+python multi_train.py -df ~/datasets/synth_A001/ --model ~/ai_models/synth_A001_$model_name
+python multi_train.py -df ~/datasets/synth_A002/ --model ~/ai_models/synth_A002_$model_name
+python multi_train.py -df ~/datasets/synth_A003/ --model ~/ai_models/synth_A003_$model_name
+python multi_train.py -df ~/datasets/synth_A004/ --model ~/ai_models/synth_A004_$model_name
+python multi_train.py -df ~/datasets/synth_A005/ --model ~/ai_models/synth_A005_$model_name
+python multi_train.py -df ~/datasets/synth_A010/ --model ~/ai_models/synth_A010_$model_name
+python multi_train.py -df ~/datasets/synth_A015/ --model ~/ai_models/synth_A015_$model_name
+python multi_train.py -df ~/datasets/synth_A020/ --model ~/ai_models/synth_A020_$model_name
+python multi_train.py -df ~/datasets/synth_A025/ --model ~/ai_models/synth_A025_$model_name
+python multi_train.py -df ~/datasets/synth_A050/ --model ~/ai_models/synth_A050_$model_name
+python multi_train.py -df ~/datasets/synth_A100/ --model ~/ai_models/synth_A100_$model_name
